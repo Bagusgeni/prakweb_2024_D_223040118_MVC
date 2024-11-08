56406f6ae7d20c9fcee287a27148faee1ad4ac78
Bagusgeni <bagusgeni0001@gmail.com>
2024-11-08 19:26:00 +0700

    Membuat Aplikasi MVC dengan PHP #5 View

--- app/core/App.php
+++ app/core/App.php
@@ -1,37 +1,41 @@
-<?php
+<?php 
 
 class App {
-    protected  $controller = 'Home';
-    protected  $method = 'index';
-    protected  $params = [];
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+    
     public function __construct()
-    {
-        $url = $this->parseURL();
-
-        //controller
-        if( file_exists('../app/controllers/' . $url[0] . '.php')){
-            $this->controller = $url[0];
-            unset($url[0]);
-        }
-
-        require_once '../app/controllers/' . $this->controller . '.php';
-        $this->controller = new $this->controller;
-
-        //method
-        if (isset($url[1])){
-            if( method_exists($this->controller, $url[1]) ){
-                $this->method = $url[1];
+        {
+            $url = $this->parseURL();
+        // Controller
+            if ($url && isset($url[0])) {
+                if ( file_exists('../app/Controllers/'. $url[0] . '.php') ) {
+                    $this->controller = $url[0];
+                    unset($url[0]);
+                }
             }
-        }
 
-        //params
-        if( !empty($url) ){
-            $this->params = array_values($url);
-            var_dump($url);
+                require_once '../app/controllers/' . $this->controller . '.php';
+                $this->controller = new $this->controller;
+
+            //Method
+            if ($url && isset($url[1])) {
+                if (method_exists($this->controller, $url[1])) {
+                    $this->method = $url[1];
+                    unset($url[1]);
+                }
+            }
+
+            // Params
+            if ($url) {
+                $this->params = array_values($url);
+            }
+    
+            // Jalankan Controller dan Method, serta kirimkan params jika ada
+            call_user_func_array([$this->controller, $this->method], $this->params);
+            
         }
-        // jalankan controller dan method, serta params jika ada
-        call_user_func_array([$this->controller, $this->method], $this->params); 
-    }
 
 
     
